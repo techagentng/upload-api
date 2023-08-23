2ccfce6044407b6c2c70c61bb779abca458e1feb
Nnamdi <nnamdi.nnah@routepayng.com>
2023-08-23 13:28:13 +0100

    seperate basePath

--- main.go
+++ main.go
@@ -1,63 +1,102 @@
 package main
 
 import (
-  "net/http"
-  "github.com/gin-gonic/gin"
-  "github.com/gin-contrib/cors"
-  _"fmt"
-	_"net/http"
-	"net/url"
+	"fmt"
+	"net/http"
+	_ "net/url"
 	"os"
 	"path/filepath"
-	_"strings"
+	"time"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
+
+// Define a custom response struct
+type FileUploadResponse struct {
+    Message     string `json:"message"`
+    FileName    string `json:"filename"`
+    FileSize    int64  `json:"filesize"`
+    ContentType string `json:"content_type"`
+    Folder      string `json:"folder"`
+}
+
 func uploadHandler(c *gin.Context) {
-	file, err := c.FormFile("file")
-	if err != nil {
-		c.JSON(http.StatusBadRequest, ErrorResponse{Error: "Bad request"})
-		return
-	}
+    fmt.Println("uploadHandler called")
 
-	filename := file.Filename
-	ext := filepath.Ext(filename)
-	allowedExts := []string{".pdf", ".docx"}
+    // Retrieve the string parameter
+    selectedFolder := c.PostForm("folder")
+    fmt.Println("Selected folder:", selectedFolder)
 
-	// Check if the file extension is allowed
-	allowed := false
-	for _, allowedExt := range allowedExts {
-		if ext == allowedExt {
-			allowed = true
-			break
-		}
-	}
-		
-	if !allowed {
-		c.JSON(http.StatusBadRequest, ErrorResponse{Error: "File type not allowed"})
-		return
-	}
-		// Get the selected folder from the request
-		selectedFolder := c.PostForm("folder")
-		if selectedFolder == "" {
-			c.JSON(http.StatusBadRequest, ErrorResponse{Error: "No folder selected"})
-			return
-	}
+    if selectedFolder == "" {
+        c.JSON(http.StatusBadRequest, ErrorResponse{Error: "No folder selected"})
+        return
+    }
 
-	// Save the file in the project directory
-	dst := filepath.Join("uploads", filename)
-	err = c.SaveUploadedFile(file, dst)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, ErrorResponse{Error: "Failed to save file"})
-		return
-	}
+    // Define the absolute path to the "uploads" directory
+    uploadsBasePath := "./"
 
-	c.JSON(http.StatusOK, FileUploadResponse{Message: "File uploaded successfully"})
+	    // Define the folder path
+		folderPath := filepath.Join(uploadsBasePath, selectedFolder)
+
+    // Create the "uploads" folder if it doesn't exist
+    if err := os.MkdirAll(uploadsBasePath, 0755); err != nil {
+        fmt.Println("Error creating subdirectory:", err)
+        c.JSON(http.StatusInternalServerError, ErrorResponse{Error: "Failed to create subdirectory"})
+        return
+    }
+
+    // Retrieve the multipart form
+    form, err := c.MultipartForm()
+
+    if err != nil {
+        c.JSON(http.StatusBadRequest, nil)
+        return
+    }
+
+    files := form.File["file"]
+    selectedFolder = form.Value["folder"][0] // Retrieve the selected folder
+    fmt.Println("formFfile", files)
+    fmt.Println(c.Request.MultipartForm.File)
+    var uploadedFiles []string // To store the uploaded file names
+    fmt.Println("Entering loop")
+
+    // Check if no files were uploaded, and create an empty PDF if necessary
+    if len(files) == 0 {
+        emptyPDFPath := filepath.Join(folderPath, selectedFolder+".pdf")
+        emptyPDF, err := os.Create(emptyPDFPath)
+        if err != nil {
+            fmt.Println("Error creating empty PDF:", err)
+            c.JSON(http.StatusInternalServerError, ErrorResponse{Error: "Failed to create empty PDF"})
+            return
+        }
+        emptyPDF.Close()
+        uploadedFiles = append(uploadedFiles, selectedFolder+".pdf")
+    }
+
+    for _, file := range files {
+        // Save the uploaded file to the specified path
+        dst := filepath.Join(folderPath, file.Filename)
+        if err := c.SaveUploadedFile(file, dst); err != nil {
+            fmt.Println("Failed to save file:", err)
+            c.JSON(http.StatusInternalServerError, ErrorResponse{Error: "Failed to save file"})
+            return
+        }
+        // fmt.Println("File uploaded:", file)
+        fmt.Println("uploads aloop", uploadedFiles)
+
+        uploadedFiles = append(uploadedFiles, file.Filename) // Store the file name
+    }
+	submissionTime := time.Now()
+    c.JSON(http.StatusOK, gin.H{
+		"message": "Files uploaded successfully", 
+		"files": uploadedFiles,
+		"submissionTime": submissionTime.Format(time.RFC3339),
+	})
 }
 
 func downloadHandler(c *gin.Context) {
 	filename := c.Param("filename")
 	filepath := filepath.Join("uploads", filename)
-
-	// Check if the file exists
 	_, err := os.Stat(filepath)
 	if os.IsNotExist(err) {
 		c.JSON(http.StatusNotFound, ErrorResponse{Error: "File not found"})
@@ -67,8 +106,52 @@ func downloadHandler(c *gin.Context) {
 	c.File(filepath)
 }
 
-type FileUploadResponse struct {
-	Message string `json:"message"`
+// func fileListHandler(c *gin.Context) {
+//     uploadsBasePath := "./"
+    
+//     files, err := os.ReadDir(uploadsBasePath)
+//     if err != nil {
+//         c.JSON(http.StatusInternalServerError, ErrorResponse{Error: "Failed to retrieve file list"})
+//         return
+//     }
+    
+//     var fileNames []string
+//     for _, file := range files {
+//         if !file.IsDir() {
+//             fileNames = append(fileNames, file.Name())
+//         }
+//     }
+    
+//     c.JSON(http.StatusOK, gin.H{"files": fileNames})
+// }
+func fileListHandler(c *gin.Context) {
+	// Retrieve the string parameter
+	selectedFolder := c.PostForm("folder")
+	fmt.Println("Selected folder:", selectedFolder)
+
+	if selectedFolder == "" {
+		c.JSON(http.StatusBadRequest, ErrorResponse{Error: "No folder selected"})
+		return
+	}
+
+    targetFolder := "your-target-folder" // Replace with the folder name you want to list
+
+    targetFolderPath := filepath.Join("./", targetFolder)
+
+    files, err := os.ReadDir(targetFolderPath)
+    if err != nil {
+        c.JSON(http.StatusInternalServerError, ErrorResponse{Error: "Failed to retrieve file list"})
+        return
+    }
+
+    var fileNames []string
+    for _, file := range files {
+        if !file.IsDir() {
+            fileNames = append(fileNames, file.Name())
+        }
+    }
+
+    c.JSON(http.StatusOK, gin.H{"files": fileNames})
 }
 
 type ErrorResponse struct {
@@ -76,44 +159,19 @@ type ErrorResponse struct {
 }
 
 func main() {
-r := gin.Default()
-r.Use(cors.Default())
-r.ForwardedByClientIP = true
-r.SetTrustedProxies([]string{"127.0.0.1"})
-
-// Define the proxy URL
-	proxyURL, _ := url.Parse("http://localhost:8080")
-
-	// Create a custom transport with the proxy settings
-	transport := &http.Transport{
-		Proxy: http.ProxyURL(proxyURL),
-	}
-
-	// Create an HTTP client with the custom transport
-	client := &http.Client{
-		Transport: transport,
-	}
-
-	// Create a route that makes a request using the custom HTTP client
-	r.GET("/", func(c *gin.Context) {
-		resp, err := client.Get("http://localhost:8080")
-		if err != nil {
-			c.String(http.StatusInternalServerError, "Error: "+err.Error())
-			return
-		}
-		defer resp.Body.Close()
-
-		// Process the response or return it as needed
-		// ...
-
-		c.String(http.StatusOK, "Request through proxy successful")
-	})
-
+	r := gin.Default()
+	r.Use(cors.Default())
+	// r.ForwardedByClientIP = true
+	// r.SetTrustedProxies([]string{"127.0.0.1"})
+	r.MaxMultipartMemory = 28 << 20 //8mib
 	// Serve uploaded files
 	r.Static("/uploads", "./uploads")
 
 	// Upload endpoint
 	r.POST("/upload", uploadHandler)
+
+	// Upload endpoint
+	r.GET("/files", fileListHandler)
 
 	// Download endpoint
 	r.GET("/download/:filename", downloadHandler)
